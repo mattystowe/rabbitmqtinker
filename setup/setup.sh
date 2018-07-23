b37ceff48266511365cec31b2b3563d586a88f89
Matthew Stowe <matt@twentysix22.com>
2018-07-23 23:07:36 +0100

    cleanup

--- setup/setup.sh
+++ setup/setup.sh
@@ -2,11 +2,11 @@
 
 echo "API Service Composer Install"
 cd apiservice
-php composer install
+composer install
 #
 echo "User Service Composer Install"
 cd ../userservice
-php composer install
+composer install
 cd ../
 #
 echo "Copy ENV to projects"
@@ -16,3 +16,7 @@ cp userservice/.env.example userservice/.env
 echo "Building Services"
 docker-compose build
 docker-compose up -d
+#
+echo "SetupDB and Run Migrations"
+#docker exec -ti rabbitmqtinker_userservice_1 sh -c "php artisan migrate:install"
+docker exec -ti rabbitmqtinker_userservice_1 sh -c "php artisan migrate --seed"
