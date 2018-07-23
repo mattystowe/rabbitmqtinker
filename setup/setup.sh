bb65794fb740bd786c2a7adb3e7a6e1179d93581
Matthew Stowe <matt@twentysix22.com>
2018-07-23 23:24:43 +0100

    Setup script includes composer

--- setup/setup.sh
+++ setup/setup.sh
@@ -1,22 +1,26 @@
 #!/bin/bash
 
+echo "Building Services"
+docker-compose build
+docker-compose up -d
+#
 echo "API Service Composer Install"
-cd apiservice
-composer install
+docker exec -ti rabbitmqtinker_app_1 sh -c "php composer.phar install --prefer-source --no-interaction"
 #
 echo "User Service Composer Install"
-cd ../userservice
-composer install
-cd ../
+docker exec -ti rabbitmqtinker_userservice_1 sh -c "php composer.phar install --prefer-source --no-interaction"
 #
 echo "Copy ENV to projects"
 cp apiservice/.env.example apiservice/.env
 cp userservice/.env.example userservice/.env
 #
-echo "Building Services"
-docker-compose build
-docker-compose up -d
-#
 echo "SetupDB and Run Migrations"
-#docker exec -ti rabbitmqtinker_userservice_1 sh -c "php artisan migrate:install"
+docker exec -ti rabbitmqtinker_userservice_1 sh -c "php artisan migrate:install"
 docker exec -ti rabbitmqtinker_userservice_1 sh -c "php artisan migrate --seed"
+#
+echo "==============================="
+echo "==============================="
+echo "==============================="
+echo "STARTED - Visit http://localhost:3000/getUser/1"
+
+docker exec -ti rabbitmqtinker_userservice_1 sh -c "php artisan rabbitmq:listen"
